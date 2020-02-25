307d233672f18cc007d00599a53c43544799311e
Mateusz Galecki <mateusz.galecki1@gmail.com>
2020-02-25 20:03:29 +0100

    Updates

---
+++ precache-manifest.848ed71910091426bfe32641862a2196.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "252e431596914f6bed13ad801fbef1fc",
+    "url": "/calendar-app/index.html"
+  },
+  {
+    "revision": "3b673a726101937ed59c",
+    "url": "/calendar-app/static/css/main.3e933993.chunk.css"
+  },
+  {
+    "revision": "4b751cd657abbbf86915",
+    "url": "/calendar-app/static/js/2.db9d67dd.chunk.js"
+  },
+  {
+    "revision": "2e7e5a466ae113f07513391c0542446b",
+    "url": "/calendar-app/static/js/2.db9d67dd.chunk.js.LICENSE"
+  },
+  {
+    "revision": "3b673a726101937ed59c",
+    "url": "/calendar-app/static/js/main.9367663d.chunk.js"
+  },
+  {
+    "revision": "f3a20602d4a972b4b14d",
+    "url": "/calendar-app/static/js/runtime-main.88ed885c.js"
+  }
+]);
